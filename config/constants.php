d0a672ae3f853a0363e8c2c6de3fdfd794603c9b
gajend <iamgajendra24@gmail.com>
2021-12-25 01:39:09 +0530

    food order web project

---
+++ config/constants.php
@@ -0,0 +1,7 @@
+<?php 
+    //Start Session
+     session_start(); 
+    $conn=mysqli_connect('localhost:3307','root','','food-order');
+    define('SITEURL', 'http://localhost/myproject/food-order-website-php-main/');
+
+?>
